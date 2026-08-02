a3e5b8f0746a815d6e58c9a48944f58650e9f512
Thaqi Ul Islam Kafi <thaqiulislamkafi@tuta.io>
2026-08-02 23:58:20 +0600

    Query Builder is implemented in Reviews module and getReviewsByUser functionality is implemented

--- src/app/modules/reviews/reviews.service.ts
+++ src/app/modules/reviews/reviews.service.ts
@@ -1,54 +1,194 @@
 import { Review } from "../../../generated/prisma/client"
 import { prisma } from "../../../lib/prisma"
+import { User } from "../../../prisma/client"
+import { QueryBuilder } from "../../utils/QueryBuilder"
 
 export const ReviewsService = {
 
-    async getReviews(){
-        const reviews = await prisma.review.findMany()
-        return reviews
-    },
+    async getReviews(query: Record<string, unknown>) {
 
-    async getReviewsByMealId(mealId: number){
-        const reviews = await prisma.review.findMany({
-            where: {
-                meal_id: mealId
+        const qb = new QueryBuilder(query)
+            .sort()
+            .paginate()
+
+        const prismaQuery = qb.build();
+
+        const page = Number(query.page) || 1;
+        const limit = Number(query.limit) || 10;
+
+        const [result, total] = await Promise.all([
+
+            await prisma.review.findMany({
+                ...prismaQuery,
+                include: {
+                    user: true,
+                    meal: true
+                }
+            }),
+
+            await prisma.review.count({
+                where: prismaQuery.where
+            })
+        ])
+
+        return {
+            data: result,
+            meta: {
+                page,
+                limit,
+                total,
+                totalPage: Math.ceil(total / limit)
             }
-        })
-        return reviews
+        }
     },
 
-    async getReviewById(id: number){
+    async getReviewsByMealId(mealId: number, query: Record<string, unknown>) {
+
+        const qb = new QueryBuilder(query)
+            .sort()
+            .paginate()
+
+        const prismaQuery = qb.build();
+        const conditionWhere = {
+            ...prismaQuery.where,
+            mealId
+        }
+
+        const page = Number(query.page) || 1;
+        const limit = Number(query.limit) || 10;
+
+        const [result, total] = await Promise.all([
+
+            await prisma.review.findMany({
+                ...prismaQuery,
+                where: conditionWhere,
+                include: {
+                    meal: true,
+                    user: true
+                }
+            }),
+
+            await prisma.review.count({
+                where: prismaQuery.where
+            })
+        ])
+
+        return {
+            data: result,
+            meta: {
+                page,
+                limit,
+                total,
+                totalPage: Math.ceil(total / limit)
+            }
+        }
+    },
+
+    async getReviewsByUser(user_id: string, query: Record<string, unknown>) {
+
+        const qb = new QueryBuilder(query)
+            .sort()
+            .paginate()
+
+        const prismaQuery = qb.build();
+
+        const page = Number(query.page) || 1;
+        const limit = Number(query.limit);
+
+        const conditionWhere = {
+            ...prismaQuery.where,
+            user_id
+        }
+
+        const [result, total] = await Promise.all([
+
+            await prisma.review.findMany({
+                ...prismaQuery,
+                where: conditionWhere,
+                include: {
+                    user: true,
+                    meal: true
+                }
+            }),
+
+            await prisma.review.count({
+                where : prismaQuery.where
+            })
+        ])
+
+        return {
+            data : result,
+            meta : {
+                page,
+                limit,
+                total,
+                totalPage : Math.ceil(total / limit)
+            }
+        }
+    },
+
+    async getReviewById(id: number) {
+
         const review = await prisma.review.findUnique({
             where: {
                 id: id
+            },
+            include: {
+                meal: true,
+                user: true
             }
         })
+
         return review
     },
 
-    async addReview(data:Review){
+    async addReview(data: Review) {
+
         const newReview = await prisma.review.create({
             data: data
         })
         return newReview
     },
 
-    async updateReview(id: number, data:Partial<Review>){
-        const updatedReview = await prisma.review.update({
-            where: {
-                id: id
-            },
-            data: data
-        })
-        return updatedReview ;
+    async updateReview(id: number, data: Partial<Review>, user: User) {
+
+        if (user.role === 'admin') {
+            return await prisma.review.update({
+                where: {
+                    id: id
+                },
+                data: data
+            })
+        }
+
+        else {
+            return await prisma.review.update({
+                where: {
+                    id,
+                    user_id: user.id
+                },
+                data
+            })
+        }
     },
 
-    async deleteReview(id:number) {
-        const deletedReview = await prisma.review.delete({
-            where: {
-                id: id
-            }
-        })
-        return deletedReview ;
+    async deleteReview(id: number, user: User) {
+
+        if (user.role === 'admin') {
+            return await prisma.review.delete({
+                where: {
+                    id: id
+                }
+            })
+        }
+
+        else {
+            return await prisma.review.delete({
+                where: {
+                    id,
+                    user_id: user.id
+                }
+            })
+        }
     }
 }
